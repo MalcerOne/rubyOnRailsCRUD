20821d5ef84c69103a5d602723fb8c1a6bb4269d
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2021-06-21 20:25:25 -0300

    CRUD FEITO, falta css

---
+++ app/models/note.rb
@@ -0,0 +1,6 @@
+class Note < ApplicationRecord
+    validates :title, presence: true,
+                    length: { minimum: 2 }
+    validates :content, presence: true,
+                    length: { minimum: 2 }
+end
